d3e4723a25b31147cf9c2d4a6af20aede3b1b387
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-11 12:27:00 +0300

    Add 002_users_up: create users table with PRIMARY KEY, NOT NULL, UNIQUE constraints then drop country_code column

---
+++ Constraints/Users-Table/002_users_up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  country_code TEXT NOT NULL,
+  user_name TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  is_admin BOOLEAN
+);
+ALTER TABLE users DROP COLUMN country_code;
