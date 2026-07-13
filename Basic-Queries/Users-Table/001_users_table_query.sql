0e70e115cdd81bb9dea2fe2fe602efd9e8325e82
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-13 22:25:24 +0300

    Add 001_users_table_query: SELECT query using BETWEEN to filter users aged 18-30

---
+++ Basic-Queries/Users-Table/001_users_table_query.sql
@@ -0,0 +1,21 @@
+SELECT
+  name,
+  age
+FROM
+  users
+WHERE
+  age BETWEEN 18 AND 30;
+
+/* RESULT
+
+name      age
+--------  ---
+Samantha  29 
+Hunter    30 
+Allan     27 
+Lance     20 
+Tiffany   28 
+Lane      27 
+Nadia     18 
+
+*/
