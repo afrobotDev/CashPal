30153aeafd46bd0b19b748b15447c2a9b30688dd
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-15 15:29:22 +0300

    Update 002_users_transactions_table_query: change subquery from = to IN to match multiple sender_ids

--- Subqueries/002_users_transactions_table_query.sql
+++ Subqueries/002_users_transactions_table_query.sql
@@ -4,7 +4,7 @@ FROM
   users
 WHERE
   is_admin = false
-  AND id = (
+  AND id IN (
     SELECT
       sender_id
     FROM
@@ -16,8 +16,12 @@ WHERE
 
 /*RESULT
 
-id  name  age  country_code  username  password            is_admin
---  ----  ---  ------------  --------  ------------------  --------
-4   Ram   42   IN            Ram11c    thisSQLcourserocks  0     
+id  name    age  country_code  username  password            is_admin
+--  ------  ---  ------------  --------  ------------------  --------
+1   David   34   US            DavidDev  insertPractice      0       
+4   Ram     42   IN            Ram11c    thisSQLcourserocks  0       
+10  Darren  15   CA            Dshan     found_me            0       
+14  Miguel  45   MX            MigDBA    secure_login        0       
+15  Nina    26   DE            NinaN     nina_notes          0  
 
 */
