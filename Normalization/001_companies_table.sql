9751f08b3e7726314b5bc28da75a27bc250dca30
YitbarekE <yitbarekendale.2024@gmail.com>
2026-07-17 18:51:40 +0300

    Add 001_companies_table: create companies table without primary key for normalization exercise

---
+++ Normalization/001_companies_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE companies (
+  name TEXT NOT NULL,
+  num_employees INTEGER NOT NULL
+);
+
+/*RESULT
+
+cid  name           type     notnull  dflt_value  pk
+---  -------------  -------  -------  ----------  --
+0    name           TEXT     1                    0 
+1    num_employees  INTEGER  1                    0 
+
+*/
